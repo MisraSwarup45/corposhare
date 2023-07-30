98e19a326e1fc209a5627ff78536b448ce523613
MisraSwarup45 <swarupkum45@gmail.com>
2023-07-30 17:31:54 +0530

    update on the login flow

---
+++ client/src/components/ProjectDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProjectDashboard.scss'
+
+const ProjectDashboard = () => {
+  return (
+    <div>ProjectDashboard</div>
+  )
+}
+
+export default ProjectDashboard;
